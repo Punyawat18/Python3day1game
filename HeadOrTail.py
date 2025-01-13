67e5851d7c43af4041e608b257b38f6f0e34a422
Punyawat18 <Punyawat_pun28@hotmail.com>
2025-01-13 10:17:51 +0700

    alr reverted it

--- HeadOrTail.py
+++ HeadOrTail.py
@@ -1,1 +1,8 @@
-#oops deleted it
+moregame = "Yes"
+
+while moregame == "Yes":
+    userchoice = input("Head or Tail")
+
+    print("You lose")
+    moregame = input("More? Yes or No")
+    #Ha it rigged
