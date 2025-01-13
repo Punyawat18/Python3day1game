66adba802271816d02a70ecbf42b4342189bab35
Punyawat18 <Punyawat_pun28@hotmail.com>
2025-01-13 10:39:53 +0700

    oops moment

--- HeadOrTail.py
+++ HeadOrTail.py
@@ -1,8 +1,1 @@
-moregame = "Yes"
-
-while moregame == "Yes":
-    userchoice = input("Head or Tail")
-
-    print("You lose")
-    moregame = input("More? Yes or No")
-    #Ha it rigged
+#truely oops moment
